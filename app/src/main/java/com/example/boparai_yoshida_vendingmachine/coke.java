37a642711b416a2a3bf5c1ba69454e1603c6858c
Sunyosh <hyoshida.liceo@gmail.com>
2023-01-31 21:56:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/boparai_yoshida_vendingmachine/coke.java
@@ -0,0 +1,14 @@
+package com.example.boparai_yoshida_vendingmachine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class coke extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coke);
+    }
+}
---
+++ app/src/main/java/com/example/boparai_yoshida_vendingmachine/sprite.java
@@ -0,0 +1,14 @@
+package com.example.boparai_yoshida_vendingmachine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sprite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sprite);
+    }
+}
